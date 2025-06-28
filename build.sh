b4f416a93df0e9c2ed8c7ff4f4228d21b0c86aa1
marcosfehlauer@protonmail.com <marcosfehlauer@protonmail.com>
2025-06-28 14:32:27 +0000

    Re-orgnize

--- build.sh
+++ build.sh
@@ -1,20 +1,11 @@
 cc=clang 
 cflags='-Os -Wall -Wextra -std=c11 -fno-strict-aliasing -fwrapv -fPIC -Wno-macro-redefined'
 
-debug=0
-
 set -xeu
 
-if [ $debug = 1 ]; then
-	$cc $cflags -O0 -g -fsanitize=address -o md4c.o -c \
-		-DMD_VERSION_MAJOR=0 \
-		-DMD_VERSION_MINOR=5 \
-		-DMD_VERSION_RELEASE=2 \
-		lib.c
-else
-	$cc $cflags -o md4c.o -c \
+$cc $cflags -o md4c.o -c \
 	-DMD_VERSION_MAJOR=0 \
-	-DMD_VERSION_MINOR=0 \
-	-DMD_VERSION_RELEASE=0 \
-	lib.c
-fi
+	-DMD_VERSION_MINOR=5 \
+	-DMD_VERSION_RELEASE=2 \
+	src/lib.c
+
--- lib.c
+++
@@ -1,4 +0,0 @@
-
-#include "src/md4c.c"
-#include "src/entity.c"
-#include "src/md4c-html.c"
