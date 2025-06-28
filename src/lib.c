63e6d299df5b508f99fb06e172de8a17c3f0c3cf
marcosfehlauer@protonmail.com <marcosfehlauer@protonmail.com>
2025-06-28 17:09:45 +0000

    Make more idiomatic, add detail structs

--- src/lib.c
+++ src/lib.c
@@ -1,3 +1,8 @@
 #include "md4c.c"
 #include "md4c-html.c"
 #include "entity.c"
+
+_Static_assert(sizeof(MD_OFFSET) == sizeof(uint32_t), "Invalid offset type");
+_Static_assert(sizeof(MD_SIZE) == sizeof(uint32_t), "Invalid size type");
+_Static_assert(sizeof(MD_CHAR) == sizeof(char), "Invalid char type");
+
